18c11fa07b3b22b7b98a66b43fc869730491101d
lamnguyenagribank <136440406+lamnguyenagribank@users.noreply.github.com>
2023-06-20 08:50:21 +0700

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -3,3 +3,4 @@ for i in range(2000, 3201): #Duyệt qua tất cả các số trong đoạn từ
     if (i%7==0) and (i%5!=0): #Kiểm tra xem số i có chia hết cho 7 và không phải là bội số của 5 không
         j.append(str(i)) #Nếu đúng, thì thêm số i vào danh sách result
 print (','.join(j)) #In ra màn hình danh sách result, các phần tử cách nhau bằng dấu phẩy
+print('test python')
