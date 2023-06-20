693b6af08834a28947a84a5c81bad0615a831fa3
lamnguyenagribank <136440406+lamnguyenagribank@users.noreply.github.com>
2023-06-20 08:53:04 +0700

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -1,3 +1,4 @@
+import streamlit as st
 j=[] #Tạo một danh sách rỗng để lưu kết quả
 for i in range(2000, 3201): #Duyệt qua tất cả các số trong đoạn từ 2000 đến 3200
     if (i%7==0) and (i%5!=0): #Kiểm tra xem số i có chia hết cho 7 và không phải là bội số của 5 không
