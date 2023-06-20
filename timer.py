d2e32b44b4a7df7bc113746a6192db3c99ddc065
lamnguyenagribank <136440406+lamnguyenagribank@users.noreply.github.com>
2023-06-20 10:20:33 +0700

    Update timer.py

--- timer.py
+++ timer.py
@@ -37,3 +37,22 @@ def count_down(count):
             marks += "✓"
 
         check_marks.config(text=marks)
+root = Tk()
+root.title("Pomodoro Timer Application")
+root.config(padx=100, pady=50, bg="#f7f5dd")
+title_label = Label(text="Timer", fg='#A020F0', bg="#f7f5dd", font=("Arial", 50))
+title_label.grid(column=1, row=0)
+canvas = Canvas(width=200, height=224, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=("Arial", 35, "bold"))
+canvas.grid(column=1, row=1)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer, bg="#e7305b", font=("arial", 15, "bold"))
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0, command = reset_timer, bg="#e7305b", font=("arial", 15, "bold"))
+reset_button.grid(column=2, row=2)
+check_marks = Label(text="", fg='#00FF00', bg="#f7f5dd", font=("arial", 25, "bold"))
+check_marks.grid(column=1, row=3)
+
+root.mainloop()
