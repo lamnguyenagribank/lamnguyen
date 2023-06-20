cb8f5807ae6f062cf16bebdd19f9cdd5cae15bb2
lamnguyenagribank <136440406+lamnguyenagribank@users.noreply.github.com>
2023-06-20 09:37:54 +0700

    Update testpython.py

--- testpython.py
+++ testpython.py
@@ -10,3 +10,19 @@ print('khong test duoc')
 st.write('test bang Write')
 menu=["test1","test 2","test 3","test 4"]
 choice = st.sidebar.selectbox('Danh mục tính năng', menu)
+import datetime
+
+print("Mời bạn vui lòng nhập ngày tháng năm sinh để tính tuổi")
+birth_day = int(input("Ngày sinh:"))
+birth_month = int(input("Tháng sinh:"))
+birth_year = int(input("Năm sinh:"))
+
+current_year = datetime.date.today().year
+current_month = datetime.date.today().month
+current_day = datetime.date.today().day
+
+age_year = current_year - birth_year
+age_month = abs(current_month-birth_month)
+age_day = abs(current_day-birth_day)
+
+print("### Tuổi của bạn chính xác là:### \n", age_year, " tuổi ", age_month, " tháng và ", age_day, " ngày")
